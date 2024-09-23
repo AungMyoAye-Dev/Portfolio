329c49aa78367f11d9af5a6ce7625eb443a5b09b
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-23 21:16:11 +0630

    add aurora background effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import ThemeChanger from "@/components/ThemeChanger";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
-      <h1>hello</h1>
+      <Hero />
       <ThemeChanger />
     </section>
   );
