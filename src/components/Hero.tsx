461f10908749c57995e49ee3c8d59a1e0b4ddfed
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-23 21:34:11 +0630

    add text effect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { AuroraBackground } from "./ui/AuroraBackground";
 
 import React from "react";
+import { TextGenerateEffect } from "./ui/TextEffect";
 
 const Hero = () => {
   return (
@@ -18,14 +19,23 @@ const Hero = () => {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="relative flex flex-col gap-4 items-center justify-center px-4"
+          className="relative flex flex-col gap-2 items-center justify-center "
         >
-          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-            Background lights are cool you know.
-          </div>
-          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-            And this, is chemical burn.
-          </div>
+          <TextGenerateEffect
+            words="Hi I'm Aung Myo Aye"
+            className="text-sm md:text-base font-semibold  text-center font-serif"
+          />
+
+          <TextGenerateEffect
+            words="A Full Stack Developer From Myanmar"
+            className="font-bold text-2xl  md:text-3xl lg:text-4xl font-serif "
+          />
+
+          <TextGenerateEffect
+            words="Tramsforming Concept into Seamless User Expriences"
+            className="text-xl  md:text-2xl font-semibold  "
+          />
+
           <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
             Debug now
           </button>
