8bd929be7936e92cd3de48aa890b420eaa3f796a
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-25 18:45:28 +0630

    add gradient background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,71 +13,66 @@ const Hero = () => {
   return (
     <section id="home">
       <AuroraBackground>
-        <div className="h-full w-full  dark:bg-grid-white/[0.03] bg-grid-gray-800/[0.2] relative flex items-center justify-center">
-          {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute -z-10 pointer-events-none inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="flex flex-col-reverse md:flex-row justify-between items-center px-6 md:px-12 gap-8 w-full"
+        >
+          <div className="relative flex flex-col gap-4 md:gap-6 text-center items-center md:text-left md:items-start w-full md:w-[50%]   ">
+            <TextGenerateEffect
+              words="Hi I'm Aung Myo Aye"
+              className="text-base md:text-lg font-semibold font-serif"
+            />
 
-          <motion.div
-            initial={{ opacity: 0.0, y: 40 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="flex flex-col-reverse md:flex-row justify-between items-center px-6 md:px-12 gap-8 w-full"
-          >
-            <div className="relative flex flex-col gap-4 md:gap-6 text-center items-center md:text-left md:items-start w-full md:w-[50%]   ">
-              <TextGenerateEffect
-                words="Hi I'm Aung Myo Aye"
-                className="text-base md:text-lg font-semibold font-serif"
-              />
+            <TextGenerateEffect
+              words="A Full Stack Developer and UI / UX Desinger From Myanmar"
+              className="font-bold text-2xl  md:text-4xl lg:text-5xl font-serif "
+            />
 
-              <TextGenerateEffect
-                words="A Full Stack Developer and UI / UX Desinger From Myanmar"
-                className="font-bold text-2xl  md:text-4xl lg:text-5xl font-serif "
-              />
-
-              <TextGenerateEffect
-                words="Tramsforming concept into Seamless User Expriences"
-                className="text-xl  md:text-2xl font-semibold  "
-                duration={2}
-              />
-              <div className="flex flex-col md:flex-row gap-8">
-                <a href="mailto:aungmyoaye.webdev@gmail.com">
-                  <Button
-                    borderRadius="1.75rem"
-                    duration={Math.floor(Math.random() * 10000) + 1000}
-                    className=" text-black dark:text-white border-neutral-400 dark:border-slate-800 bg-slate-300 dark:bg-gray-800 text-base opacity-95 hover:bg-purple-400 px-6 py-3 shadow-md"
-                  >
-                    Get in touch
-                  </Button>
-                </a>
+            <TextGenerateEffect
+              words="Tramsforming concept into Seamless User Expriences"
+              className="text-xl  md:text-2xl font-semibold  "
+              duration={2}
+            />
+            <div className="flex flex-col md:flex-row gap-8">
+              <a href="mailto:aungmyoaye.webdev@gmail.com">
                 <Button
                   borderRadius="1.75rem"
                   duration={Math.floor(Math.random() * 10000) + 1000}
-                  className=" text-black dark:text-white border-neutral-400 dark:border-slate-800   text-base bg-opacity-95 bg-purple-700 px-6 py-3 shadow-md"
+                  className=" text-black dark:text-white border-neutral-400 dark:border-slate-800 bg-slate-300 dark:bg-gray-800 text-base opacity-95 hover:bg-purple-400 px-6 py-3 shadow-md"
                 >
-                  Download My Resume
+                  Get in touch
                 </Button>
-              </div>
-            </div>
-            <div>
+              </a>
               <Button
                 borderRadius="1.75rem"
-                duration={Math.floor(Math.random() * 10000) + 10000}
-                className="relative size-60 md:size-80  "
+                duration={Math.floor(Math.random() * 10000) + 1000}
+                className=" text-black dark:text-white border-neutral-400 dark:border-slate-800   text-base bg-opacity-95 bg-purple-700 px-6 py-3 shadow-md"
               >
-                <Image
-                  src={"/profile.svg"}
-                  fill
-                  alt="Aung Myo Aye's profile photo"
-                  className="object-cover  "
-                />
+                Download My Resume
               </Button>
             </div>
-          </motion.div>
-        </div>
+          </div>
+          <div>
+            <Button
+              borderRadius="1.75rem"
+              duration={Math.floor(Math.random() * 10000) + 10000}
+              className="relative size-60 md:size-80  "
+            >
+              <Image
+                src={"/profile.svg"}
+                fill
+                alt="Aung Myo Aye's profile photo"
+                className="object-cover  "
+              />
+            </Button>
+          </div>
+        </motion.div>
       </AuroraBackground>
     </section>
   );
--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -58,9 +58,10 @@ export const BentoGridItem = ({
           )}
         </div>
 
+        {/* Gradient background */}
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
         )}
 
