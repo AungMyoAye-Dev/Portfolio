90c7198ac049c207fbf233c55bbff47dc30bc295
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-26 19:18:54 +0630

    add 3d pin card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { PinContainer } from "./ui/3Dpin";
 
 const Projects = () => {
   return (
@@ -7,6 +9,27 @@ const Projects = () => {
         My Small Section Of{" "}
         <span className="text-purple-300">Recent Projects</span>
       </h1>
+
+      <main>
+        <div className="h-[40rem] w-full flex items-center justify-center ">
+          <PinContainer
+            title="/ui.aceternity.com"
+            href="https://twitter.com/mannupaaji"
+          >
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                Aceternity UI
+              </h3>
+              <div className="text-base !m-0 !p-0 font-normal">
+                <span className="text-slate-500 ">
+                  Customizable Tailwind CSS and Framer Motion Components.
+                </span>
+              </div>
+              <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+            </div>
+          </PinContainer>
+        </div>
+      </main>
     </section>
   );
 };
