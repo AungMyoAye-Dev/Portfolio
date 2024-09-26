71ab49de81ce15fc6d1648006fac8a159c672109
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-26 21:27:25 +0630

    add project data

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/BentoGrid";
-import { Griditems } from "@/data";
+import { gridItems } from "@/data";
 
 const About = () => {
   return (
     <section className="py-20 px-10 md:px-12" id="about">
       <BentoGrid className=" mx-auto">
-        {Griditems.map((item) => (
+        {gridItems.map((item) => (
           <BentoGridItem
             key={item.id}
             id={item.id}
