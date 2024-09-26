4c0ec4d91bfad77788698b2345d5e2c5d33bf1a7
AungMyoAye-Dev <aungmyoaye.webdev@gmail.com>
2024-09-26 21:44:07 +0630

    add project data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { projects } from "@/data";
 import React from "react";
-import { PinContainer } from "./ui/3Dpin";
+import Card from "./ui/3Dcard";
 
 const Projects = () => {
   return (
@@ -10,25 +11,10 @@ const Projects = () => {
         <span className="text-purple-300">Recent Projects</span>
       </h1>
 
-      <main>
-        <div className="h-[40rem] w-full flex items-center justify-center ">
-          <PinContainer
-            title="/ui.aceternity.com"
-            href="https://twitter.com/mannupaaji"
-          >
-            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                Aceternity UI
-              </h3>
-              <div className="text-base !m-0 !p-0 font-normal">
-                <span className="text-slate-500 ">
-                  Customizable Tailwind CSS and Framer Motion Components.
-                </span>
-              </div>
-              <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-            </div>
-          </PinContainer>
-        </div>
+      <main className="grid md:grid-cols-2 gap-4 mx-auto place-items-center">
+        {projects.map((project) => (
+          <Card />
+        ))}
       </main>
     </section>
   );
---
+++ src/components/ui/3Dcard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { PinContainer } from "./3Dpin";
+
+const Card = () => {
+  return (
+    <div className="h-[25rem] sm:w-96 w-[80vw] flex items-center justify-center bg-lime-400">
+      <PinContainer
+        title="/ui.aceternity.com"
+        href="https://twitter.com/mannupaaji"
+      >
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 sm:w-96 w-[80vw] h-[20vh] lg:h-[30vh] mb-10 ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Aceternity UI
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
+    </div>
+  );
+};
+
+export default Card;
